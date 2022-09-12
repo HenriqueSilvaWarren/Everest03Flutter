e824490b2551f6b8f1be7a09377bdd940325a2a8
Henrique Silva <henrique.silva@warren.com.br>
2022-09-12 11:09:45 -0300

    feat: created the list with all the cryptocoin and implemented the hide monetary values feature

--- lib/presenter/wallet/wallet_screen.dart
+++ lib/presenter/wallet/wallet_screen.dart
@@ -9,111 +9,362 @@ class WalletScreen extends StatefulWidget {
 }
 
 class _WalletScreenState extends State<WalletScreen> {
+  bool isHidden = false;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      bottomNavigationBar: BottomNavigationBar(
+        items: const [
+          BottomNavigationBarItem(
+            label: 'Portfólio',
+            icon: ImageIcon(
+              AssetImage("assets/icons/warren_active.png"),
+            ),
+          ),
+          BottomNavigationBarItem(
+            label: "Movimentações",
+            icon: ImageIcon(
+              AssetImage("assets/icons/warren_inactive.png"),
+            ),
+          ),
+        ],
+      ),
       body: SafeArea(
         child: Column(
           children: [
             Container(
               margin: const EdgeInsets.only(
+                top: 10,
                 left: 15,
                 right: 15,
                 bottom: 30,
               ),
-              child: Padding(
-                padding: const EdgeInsets.only(
-                  left: 16,
-                  right: 16,
-                  bottom: 24,
-                ),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Text(
-                          "Carteira",
-                          style: GoogleFonts.montserrat(
-                            fontSize: 32,
-                            fontWeight: FontWeight.w700,
-                          ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        "Cripto",
+                        style: GoogleFonts.montserrat(
+                          fontSize: 32,
+                          color: const Color.fromARGB(255, 224, 43, 87),
+                          fontWeight: FontWeight.w700,
                         ),
-                        IconButton(
-                          padding: EdgeInsets.zero,
-                          onPressed: () {},
-                          icon: const Icon(
-                            Icons.remove_red_eye,
-                            size: 22,
-                          ),
-                        ),
-                      ],
-                    ),
-                    Text(
-                      "U\$ 1.000,0",
-                      style: GoogleFonts.montserrat(
-                        fontSize: 32,
-                        fontWeight: FontWeight.w700,
                       ),
-                    ),
-                  ],
-                ),
+                      IconButton(
+                        padding: EdgeInsets.zero,
+                        onPressed: () {
+                          setState(() {
+                            isHidden = !isHidden;
+                          });
+                        },
+                        icon: Icon(
+                          !isHidden ? Icons.visibility : Icons.visibility_off,
+                          size: 22,
+                        ),
+                      ),
+                    ],
+                  ),
+                  AnimatedSwitcher(
+                    duration: const Duration(milliseconds: 250),
+                    child: !isHidden
+                        ? Text(
+                            "R\$ 14.798,00",
+                            style: GoogleFonts.montserrat(
+                              fontSize: 32,
+                              fontWeight: FontWeight.w700,
+                            ),
+                          )
+                        : Container(
+                            height: 39,
+                            width: 210,
+                            decoration: BoxDecoration(
+                              color: Colors.grey.shade500,
+                              borderRadius: BorderRadius.circular(5),
+                            ),
+                          ),
+                  ),
+                  Text(
+                    "Valor total de moedas",
+                    style: GoogleFonts.sourceSansPro(
+                        fontSize: 17,
+                        fontWeight: FontWeight.w400,
+                        color: const Color.fromARGB(255, 117, 118, 128)),
+                  )
+                ],
               ),
             ),
             SizedBox(
               width: MediaQuery.of(context).size.width,
-              height: MediaQuery.of(context).size.height * 0.7,
+              height: MediaQuery.of(context).size.height * 0.65,
               child: ListView(
+                physics: const BouncingScrollPhysics(),
                 children: [
                   ListTile(
+                    horizontalTitleGap: 0,
                     shape: const Border(
                       top: BorderSide(
                         color: Color.fromARGB(255, 227, 228, 235),
                       ),
                     ),
-                    leading: const CircleAvatar(
-                      backgroundColor: Color.fromARGB(255, 47, 47, 51),
-                    ),
-                    title: Text(
-                      "BTC",
-                      style: GoogleFonts.sourceSansPro(
-                        fontWeight: FontWeight.w400,
-                        fontSize: 20,
+                    leading: ClipRRect(
+                      borderRadius: BorderRadius.circular(100),
+                      child: Image.asset(
+                        "assets/icons/bitcoin.png",
+                        height: 48,
                       ),
                     ),
-                    subtitle: Text(
-                      "Bitcoin",
-                      style: GoogleFonts.sourceSansPro(
-                        fontWeight: FontWeight.w400,
-                        fontSize: 15,
+                    title: Padding(
+                      padding: const EdgeInsets.only(left: 8.0),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            "BTC",
+                            style: GoogleFonts.sourceSansPro(
+                              fontWeight: FontWeight.w400,
+                              fontSize: 20,
+                            ),
+                          ),
+                          AnimatedSwitcher(
+                            duration: const Duration(milliseconds: 250),
+                            child: !isHidden
+                                ? Text(
+                                    "R\$ 6.557,00",
+                                    style: GoogleFonts.sourceSansPro(
+                                      fontSize: 20,
+                                      fontWeight: FontWeight.w400,
+                                    ),
+                                  )
+                                : Container(
+                                    height: 20,
+                                    width: 95,
+                                    decoration: BoxDecoration(
+                                      color: Colors.grey.shade500,
+                                      borderRadius: BorderRadius.circular(5),
+                                    ),
+                                  ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    subtitle: Padding(
+                      padding: const EdgeInsets.only(top: 4.0, left: 8.0),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            "Bitcoin",
+                            style: GoogleFonts.sourceSansPro(
+                              fontWeight: FontWeight.w400,
+                              fontSize: 15,
+                            ),
+                          ),
+                          Text(
+                            "0.65 BTC",
+                            style: GoogleFonts.sourceSansPro(
+                              fontSize: 15,
+                              fontWeight: FontWeight.w400,
+                            ),
+                          )
+                        ],
                       ),
                     ),
                     trailing: Column(
-                      crossAxisAlignment: CrossAxisAlignment.end,
-                      children: [
-                        Text(
-                          'US\$ 6557,00',
-                          style: GoogleFonts.sourceSansPro(
-                            fontWeight: FontWeight.w400,
-                            fontSize: 20,
+                      children: const [
+                        Padding(
+                          padding: EdgeInsets.only(
+                            top: 2,
+                            bottom: 6,
+                            left: 9,
+                            right: 8,
+                          ),
+                          child: Icon(
+                            Icons.chevron_right,
+                            size: 24,
+                            color: Color.fromARGB(255, 117, 118, 128),
                           ),
                         ),
-                        Container(
-                          margin: const EdgeInsets.only(top: 4),
-                          decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(16),
-                            color: const Color.fromARGB(255, 160, 244, 224),
-                          ),
-                          width: 52,
-                          height: 20,
-                          alignment: Alignment.center,
-                          child: Text(
-                            "+75%",
+                      ],
+                    ),
+                  ),
+                  ListTile(
+                    horizontalTitleGap: 0,
+                    shape: const Border(
+                      top: BorderSide(
+                        color: Color.fromARGB(255, 227, 228, 235),
+                      ),
+                    ),
+                    leading: ClipRRect(
+                      borderRadius: BorderRadius.circular(100),
+                      child: Image.asset(
+                        "assets/icons/ethereum.png",
+                        height: 48,
+                      ),
+                    ),
+                    title: Padding(
+                      padding: const EdgeInsets.only(left: 8.0),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            "ETH",
                             style: GoogleFonts.sourceSansPro(
                               fontWeight: FontWeight.w400,
-                              fontSize: 12,
+                              fontSize: 20,
                             ),
+                          ),
+                          AnimatedSwitcher(
+                            duration: const Duration(milliseconds: 250),
+                            child: !isHidden
+                                ? Text(
+                                    "R\$ 7.996,00",
+                                    style: GoogleFonts.sourceSansPro(
+                                      fontSize: 20,
+                                      fontWeight: FontWeight.w400,
+                                    ),
+                                  )
+                                : Container(
+                                    height: 20,
+                                    width: 95,
+                                    decoration: BoxDecoration(
+                                      color: Colors.grey.shade500,
+                                      borderRadius: BorderRadius.circular(5),
+                                    ),
+                                  ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    subtitle: Padding(
+                      padding: const EdgeInsets.only(
+                        top: 4.0,
+                        left: 8,
+                      ),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            "Ethereum",
+                            style: GoogleFonts.sourceSansPro(
+                              fontWeight: FontWeight.w400,
+                              fontSize: 15,
+                            ),
+                          ),
+                          Text(
+                            "0.94 ETH",
+                            style: GoogleFonts.sourceSansPro(
+                              fontSize: 15,
+                              fontWeight: FontWeight.w400,
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                    trailing: Column(
+                      children: const [
+                        Padding(
+                          padding: EdgeInsets.only(
+                            top: 2,
+                            bottom: 6,
+                            left: 9,
+                            right: 4,
+                          ),
+                          child: Icon(
+                            Icons.chevron_right,
+                            size: 24,
+                            color: Color.fromARGB(255, 117, 118, 128),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  ListTile(
+                    horizontalTitleGap: 0,
+                    shape: const Border(
+                      top: BorderSide(
+                        color: Color.fromARGB(255, 227, 228, 235),
+                      ),
+                    ),
+                    leading: ClipRRect(
+                      borderRadius: BorderRadius.circular(100),
+                      child: Image.asset(
+                        "assets/icons/litecoin.png",
+                        height: 48,
+                      ),
+                    ),
+                    title: Padding(
+                      padding: const EdgeInsets.only(left: 8.0),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            "LTC",
+                            style: GoogleFonts.sourceSansPro(
+                              fontWeight: FontWeight.w400,
+                              fontSize: 20,
+                            ),
+                          ),
+                          AnimatedSwitcher(
+                            duration: const Duration(milliseconds: 250),
+                            child: !isHidden
+                                ? Text(
+                                    "R\$ 245,00",
+                                    style: GoogleFonts.sourceSansPro(
+                                      fontSize: 20,
+                                      fontWeight: FontWeight.w400,
+                                    ),
+                                  )
+                                : Container(
+                                    height: 20,
+                                    width: 79,
+                                    decoration: BoxDecoration(
+                                      color: Colors.grey.shade500,
+                                      borderRadius: BorderRadius.circular(5),
+                                    ),
+                                  ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    subtitle: Padding(
+                      padding: const EdgeInsets.only(top: 4.0, left: 8.0),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            "Litecoin",
+                            style: GoogleFonts.sourceSansPro(
+                              fontWeight: FontWeight.w400,
+                              fontSize: 15,
+                            ),
+                          ),
+                          Text(
+                            "0.82 LTC",
+                            style: GoogleFonts.sourceSansPro(
+                              fontSize: 15,
+                              fontWeight: FontWeight.w400,
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                    trailing: Column(
+                      children: const [
+                        Padding(
+                          padding: EdgeInsets.only(
+                            top: 2,
+                            bottom: 6,
+                            left: 9,
+                            right: 4,
+                          ),
+                          child: Icon(
+                            Icons.chevron_right,
+                            size: 24,
+                            color: Color.fromARGB(255, 117, 118, 128),
                           ),
                         ),
                       ],
