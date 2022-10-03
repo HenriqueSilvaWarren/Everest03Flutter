17531be3cbf79f70614ed3199cec0a341cac8742
Henrique Silva <henrique.silva@warren.com.br>
2022-10-03 16:06:07 -0300

    refactor: changed the exchanged screen to stateless

--- lib/app/presenter/screens/exchanged/exchanged_screen.dart
+++ lib/app/presenter/screens/exchanged/exchanged_screen.dart
@@ -4,17 +4,11 @@ import 'package:flutter/scheduler.dart' show timeDilation;
 import 'widgets/body_exchanged_screen.dart';
 import 'widgets/exchanged_app_bar.dart';
 
-class ExchangedScreen extends StatefulWidget {
+class ExchangedScreen extends StatelessWidget {
   const ExchangedScreen({Key? key}) : super(key: key);
 
   static String route = '/exchanged-screen';
 
-  @override
-  State<ExchangedScreen> createState() => _ExchangedScreenState();
-}
-
-class _ExchangedScreenState extends State<ExchangedScreen>
-    with SingleTickerProviderStateMixin {
   @override
   Widget build(BuildContext context) {
     timeDilation = 1;
